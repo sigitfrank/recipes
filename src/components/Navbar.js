04210d59ff14172170ce3852cfec9ac905bbc529
sigitfrank <sigittt53@gmail.com>
2021-05-04 11:06:40 +0700

    add reducer and action types for navbar, update css active link in navbar.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,58 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useReducer } from 'react'
+import { Link } from 'react-router-dom'
 import { BsSearch } from 'react-icons/bs'
 import { RiMenu2Fill } from 'react-icons/ri'
+import { TYPING_SEARCH_RECIPES, SEARCHING_RECIPES, CHANGE_MENU } from '../action-types/Navbar'
+import { NavbarReducer } from '../reducers/NavbarReducer'
 import '../css/navbar.css'
-function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            <img src="/assets/logo.png" alt="bagi-resep" />
-          </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <RiMenu2Fill />
-          </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto py-3 ms-4 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/recipes">Recipes</Link>
-              </li>
-              <li className="nav-item">
-              <Link className="nav-link" to="/blogs">Blog</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/">About Us</Link>
-              </li>
-            </ul>
-            <form className="d-flex search-form">
-              <div className="input-group me-3">
-                <button className="btn ms-n5" type="button">
-                  <BsSearch />
-                </button>
-                <input className="form-control" type="search" placeholder="ex: Spaghetti carbonara" />
-              </div>
-              <button className="btn sign-in" data-bs-toggle="modal" data-bs-target="#SignInModal" type="button">Sign in</button>
-            </form>
-          </div>
+const initialNavbarState = {
+  activeMenu: 'Home',
+  search: ''
+}
+
+function Navbar() {
+
+  const [navbarState, navbarDispatcher] = useReducer(NavbarReducer, initialNavbarState)
+  const { activeMenu, search } = navbarState
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <img src="/assets/logo.png" alt="bagi-resep" />
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <RiMenu2Fill />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto py-3 ms-4 mb-lg-0">
+            <li className="nav-item">
+              <Link className={`nav-link ${activeMenu === 'Home' && 'active'}`} onClick={() => navbarDispatcher({ type: CHANGE_MENU, payload: 'Home' })} to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link ${activeMenu === 'Recipes' && 'active'}`} onClick={() => navbarDispatcher({ type: CHANGE_MENU, payload: 'Recipes' })} to="/recipes">Recipes</Link>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link ${activeMenu === 'Blog' && 'active'}`} onClick={() => navbarDispatcher({ type: CHANGE_MENU, payload: 'Blog' })} to="/blogs">Blog</Link>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link ${activeMenu === 'About Us' && 'active'}`} onClick={() => navbarDispatcher({ type: CHANGE_MENU, payload: 'About Us' })} to="/">About Us</Link>
+            </li>
+          </ul>
+          <form className="d-flex search-form">
+            <div className="input-group me-3">
+              <button className="btn ms-n5" type="button" onClick={() => navbarDispatcher({ type: SEARCHING_RECIPES, payload: search })}>
+                <BsSearch />
+              </button>
+              <input className="form-control" value={search} onChange={(e) => navbarDispatcher({ type: TYPING_SEARCH_RECIPES, payload: e.target.value })} type="search" placeholder="ex: Spaghetti carbonara" />
+            </div>
+            <button className="btn sign-in" data-bs-toggle="modal" data-bs-target="#SignInModal" type="button">Sign in</button>
+          </form>
         </div>
-      </nav>
-    )
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
---
+++ src/reducers/NavbarReducer.js
@@ -0,0 +1,22 @@
+
+import { TYPING_SEARCH_RECIPES, SEARCHING_RECIPES, CHANGE_MENU } from '../action-types/Navbar'
+const NavbarReducer = (state = {}, action) => {
+    if (action.type === TYPING_SEARCH_RECIPES) {
+
+        return { ...state, search: action.payload }
+    }
+
+    if (action.type === SEARCHING_RECIPES) {
+        alert(`Searching recipes: ${action.payload}`)
+        return { ...state, search: '' }
+    }
+
+    if (action.type === CHANGE_MENU) {
+        console.log(state)
+        console.log(action)
+        return { ...state, activeMenu: action.payload }
+    }
+    return state
+}
+
+export { NavbarReducer }
