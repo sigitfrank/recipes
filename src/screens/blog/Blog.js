9b8478cf2ba704e760efc92d6190ff409e02e470
sigitfrank <sigittt53@gmail.com>
2021-05-07 10:05:39 +0700

    finishing blog page

--- src/screens/blog/Blog.js
+++ src/screens/blog/Blog.js
@@ -31,9 +31,9 @@ function Blog() {
 
                 <section className="blog-main">
                     <div className="row">
-                        <div className="col-md-8">
+                        <div className="col-md-9">
                             <div className="row">
-                                <div className="col-md-6">
+                                <div className="col-md-4">
                                     <div className="blog">
                                         <img src="/assets/blog/blog-lists/1.png" alt="blog-list" className="img-fluid" />
                                         <div className="content">
@@ -43,7 +43,37 @@ function Blog() {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="col-md-6">
+                                <div className="col-md-4">
+                                    <div className="blog">
+                                        <img src="/assets/blog/blog-lists/2.png" alt="blog-list" className="img-fluid" />
+                                        <div className="content">
+                                            <span className="category">Food News</span>
+                                            <p className="title">How McDonald's Makes Money: Franchising Fast Food</p>
+                                            <span className="author">M Yunan Adiyaksatama - March 2021</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="blog">
+                                        <img src="/assets/blog/blog-lists/3.png" alt="blog-list" className="img-fluid" />
+                                        <div className="content">
+                                            <span className="category">Food News</span>
+                                            <p className="title">How McDonald's Makes Money: Franchising Fast Food</p>
+                                            <span className="author">M Yunan Adiyaksatama - March 2021</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="blog">
+                                        <img src="/assets/blog/blog-lists/4.png" alt="blog-list" className="img-fluid" />
+                                        <div className="content">
+                                            <span className="category">Food News</span>
+                                            <p className="title">How McDonald's Makes Money: Franchising Fast Food</p>
+                                            <span className="author">M Yunan Adiyaksatama - March 2021</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-4">
                                     <div className="blog">
                                         <img src="/assets/blog/blog-lists/1.png" alt="blog-list" className="img-fluid" />
                                         <div className="content">
@@ -53,9 +83,19 @@ function Blog() {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="col-md-6">
+                                <div className="col-md-4">
                                     <div className="blog">
-                                        <img src="/assets/blog/blog-lists/1.png" alt="blog-list" className="img-fluid" />
+                                        <img src="/assets/blog/blog-lists/2.png" alt="blog-list" className="img-fluid" />
+                                        <div className="content">
+                                            <span className="category">Food News</span>
+                                            <p className="title">How McDonald's Makes Money: Franchising Fast Food</p>
+                                            <span className="author">M Yunan Adiyaksatama - March 2021</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="blog">
+                                        <img src="/assets/blog/blog-lists/3.png" alt="blog-list" className="img-fluid" />
                                         <div className="content">
                                             <span className="category">Food News</span>
                                             <p className="title">How McDonald's Makes Money: Franchising Fast Food</p>
@@ -65,16 +105,42 @@ function Blog() {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-4">
+                        <div className="col-md-3">
                             <div className="social-media-links">
-                                <span>Follow Us</span>
+                                <span className="follow-us">Follow Us</span>
                                 <ul>
-                                    <li><FaFacebook className="social-icon" /> Bagi Resep</li>
-                                    <li><FaTwitter className="social-icon" /> @bagiResep</li>
-                                    <li><FaInstagram className="social-icon" /> @bagiResep</li>
+                                    <li><FaFacebook className="social-icon" /> <span className="social-title"> Bagi Resep</span></li>
+                                    <li><FaTwitter className="social-icon" /> <span className="social-title">@bagiResep</span></li>
+                                    <li><FaInstagram className="social-icon" /> <span className="social-title">@bagiResep</span></li>
                                 </ul>
                             </div>
-
+                            <div className="top-articles">
+                                <p className="top-category">Top Articles</p>
+                                <div className="article">
+                                    <img src="/assets/blog/blog-lists/2.png" alt="blog-list" className="img-fluid" />
+                                    <div className="content">
+                                        <span className="category">Food News</span>
+                                        <p className="title">How McDonald's Makes Money: Franchising Fast Food</p>
+                                        <span className="author">M Yunan Adiyaksatama - March 2021</span>
+                                    </div>
+                                </div>
+                                <div className="article">
+                                    <img src="/assets/blog/blog-lists/2.png" alt="blog-list" className="img-fluid" />
+                                    <div className="content">
+                                        <span className="category">Food News</span>
+                                        <p className="title">How McDonald's Makes Money: Franchising Fast Food</p>
+                                        <span className="author">M Yunan Adiyaksatama - March 2021</span>
+                                    </div>
+                                </div>
+                                <div className="article">
+                                    <img src="/assets/blog/blog-lists/2.png" alt="blog-list" className="img-fluid" />
+                                    <div className="content">
+                                        <span className="category">Food News</span>
+                                        <p className="title">How McDonald's Makes Money: Franchising Fast Food</p>
+                                        <span className="author">M Yunan Adiyaksatama - March 2021</span>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </section>
