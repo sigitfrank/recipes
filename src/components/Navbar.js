c0266b29046de65ff614a43b95e30bf88f4b21d5
sigitfrank <sigittt53@gmail.com>
2021-05-10 17:14:56 +0700

    add add recipes link on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,6 +38,9 @@ function Navbar() {
             <li className="nav-item">
               <NavLink className="nav-link" activeClassName='active' to="/about-us">About Us</NavLink>
             </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName='active' to="/add-recipes">Add Recipes</NavLink>
+            </li>
           </ul>
           <form className="d-flex search-form" onSubmit={(e)=>e.preventDefault()}>
             <div className="input-group me-3">
