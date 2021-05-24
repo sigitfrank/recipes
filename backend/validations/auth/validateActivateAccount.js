79d3421c1a0f9f35bbc934a364707e3a4f06e1bf
sigitfrank <sigittt53@gmail.com>
2021-05-24 16:37:21 +0700

    refactor http status code

--- backend/validations/auth/validateActivateAccount.js
+++ backend/validations/auth/validateActivateAccount.js
@@ -16,7 +16,7 @@ const validateActivateAccount = [
     ,
     (req, res, next) => {
         const errors = validationResult(req);
-        if (!errors.isEmpty()) return res.status(200).json({ success: false, errors: errors.array() });
+        if (!errors.isEmpty()) return res.status(400).json({ success: false, errors: errors.array() });
         next();
     },
 ]
