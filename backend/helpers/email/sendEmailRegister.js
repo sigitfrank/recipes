f76901c261f60a1afd69ea77e7cbd26aa0662573
sigitfrank <sigittt53@gmail.com>
2021-05-25 14:33:24 +0700

    update base url front end in env backend and update sendEmailRegister js

--- backend/helpers/email/sendEmailRegister.js
+++ backend/helpers/email/sendEmailRegister.js
@@ -18,7 +18,7 @@ const sendEmail = (userData) => {
         <p>
             Thank you for registering to our App. Hope you enjoy it! Please activate your account by clicking the link below
         </p>
-        <p style="font-weight:bold;"><a href="http://localhost:3000/activate/${email}/${token}">Activate Account</a></p>
+        <p style="font-weight:bold;"><a href="${process.env.BASE_URL_FRONT_END}/activate/${email}/${token}">Activate Account</a></p>
         `
     }
 
